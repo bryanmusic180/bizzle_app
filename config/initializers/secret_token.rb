63345179d30f85163d82af8aeea1ae2aaf046ad5
Bryan <dynamixmm@gmail.com>
2011-05-03 15:38:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BizzleApp::Application.config.secret_token = 'cdb338ad1c591f2d7bb32147f36f41e2063264d1fd2c68e0a073a746f231eb433119dad70b1a35a470830e4427fe89928dcd986007944832112235a1379bc38f'
