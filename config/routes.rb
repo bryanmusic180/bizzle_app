624bdd4c598b90ae396d23346cc36a67b6783fc0
Bryan <dynamixmm@gmail.com>
2011-05-08 00:51:10 -0700

    Added a Sessions controller and a signin view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 BizzleApp::Application.routes.draw do
   
-  
+
  resources :users
+ resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
   
@@ -9,7 +10,8 @@ BizzleApp::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
-
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   
 
